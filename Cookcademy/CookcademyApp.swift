2a9a773f410eff36ab7c094b72d77cfd2c9161b1
highness-ru <highness.ru@gmail.com>
2026-03-05 20:16:14 +0000

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Анна Герман on 05.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
